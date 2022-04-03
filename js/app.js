903db62a19c1a4b1dd56e83a23a19393b2bd1b08
erikawww <welschenerika@gmail.com>
2022-04-03 14:50:21 -0300

    loading, chat and menu

--- js/app.js
+++ js/app.js
@@ -3,14 +3,17 @@ let btnIngresar = document.querySelector('.ingresar')
 let btnRegistrar = document.querySelector('.registrar')
 let aRegistro = document.querySelector('#aRegistro')
 let aIngreso = document.querySelector('#aIngreso')
-
+let enviar = document.querySelector('#enviar')
+let menu = document.querySelector('.hamburger')
 // pantallas
 let inicio = document.querySelector('.inicio')
 let ingreso  = document.querySelector('.ingreso')
 let registro = document.querySelector('.registro')
+let chat = document.querySelector('.container-chat')
 // Todas las pantallas
 let pantallas = document.querySelectorAll('.screen')
-
+// componentes
+let navLinks = document.querySelector('.nav-links')
 
 
 
@@ -37,3 +40,17 @@ aRegistro.addEventListener('click', ()=>{
     nextScreen(registro)
 })
 
+// abrir y cerrar menu
+let abrirMenu = ()=>{
+    menu.addEventListener('click', ()=>{
+        menu.classList.toggle('is-active')
+        if (menu.classList.contains('is-active')){
+            navLinks.classList.add('active-menu')
+        }else{
+            navLinks.classList.remove('active-menu')
+            
+        }
+    })
+}
+
+abrirMenu()
